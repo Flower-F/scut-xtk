3395f332183be93f6ef5f00f817e8354c5dbec15
FlowerF <1647231392@qq.com>
2023-01-12 16:46:46 +0800

    feat: theme toggle

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,11 +1,13 @@
 import { Bars3Icon } from '@heroicons/react/24/solid';
 import Link from 'next/link';
 import Image from 'next/image';
+import { useEffect } from 'react';
+import { useLocalStorageState } from 'ahooks';
 import LogoImage from '../../public/images/logo.png';
 
 export function NavBar() {
   return (
-    <div className='navbar bg-primary'>
+    <header className='navbar bg-primary'>
       <div className='navbar-start w-full'>
         <Link href='/' className='rounded-md px-2'>
           <Image src={LogoImage} className='h-16 w-auto sm:h-20' alt='Logo' priority />
@@ -23,6 +25,9 @@ export function NavBar() {
           <li>
             <Link href='/mine'>我的</Link>
           </li>
+          <li>
+            <ThemeToggleButton />
+          </li>
         </ul>
       </div>
 
@@ -31,7 +36,7 @@ export function NavBar() {
           <label tabIndex={0} className='btn-ghost btn'>
             <Bars3Icon className='h-6 w-6 text-gray-100' />
           </label>
-          <ul className='dropdown-content menu rounded-box menu-compact mt-3 w-52 bg-base-100 p-2 shadow'>
+          <ul className='dropdown-content menu rounded-box menu-compact mt-3 w-24 bg-base-200 p-2 shadow'>
             <li>
               <Link href='/'>首页</Link>
             </li>
@@ -41,9 +46,34 @@ export function NavBar() {
             <li>
               <Link href='/mine'>我的</Link>
             </li>
+            <li>
+              <ThemeToggleButton />
+            </li>
           </ul>
         </div>
       </div>
-    </div>
+    </header>
+  );
+}
+
+const THEMES = ['corporate', 'night', 'winter', 'light', 'business', 'dark'];
+function ThemeToggleButton() {
+  const [themeIndex, setThemeIndex] = useLocalStorageState('scut-xtk-theme', {
+    defaultValue: 0,
+  });
+
+  function handleThemeChange() {
+    const nextThemeIndex = (themeIndex + 1) % THEMES.length;
+    setThemeIndex(nextThemeIndex);
+  }
+
+  useEffect(() => {
+    document.documentElement.setAttribute('data-theme', THEMES[themeIndex] || 'corporate');
+  }, [themeIndex]);
+
+  return (
+    <label tabIndex={0} className='cursor-pointer' onClick={() => handleThemeChange()}>
+      主题
+    </label>
   );
 }
