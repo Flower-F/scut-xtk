0112062185ea81854a0ab4e4ad5015ebcac55dba
FlowerF <1647231392@qq.com>
2023-01-31 21:30:51 +0800

    refactor: refactor next-auth

--- src/server/auth.ts
+++ src/server/auth.ts
@@ -1,20 +1,115 @@
 import type { GetServerSidePropsContext } from 'next';
-import { unstable_getServerSession } from 'next-auth';
-import { authOptions } from '../pages/api/auth/[...nextauth]';
+import { getServerSession, type NextAuthOptions, type DefaultSession } from 'next-auth';
+import CredentialsProvider from 'next-auth/providers/credentials';
+import { PrismaAdapter } from '@next-auth/prisma-adapter';
+import { compare } from 'bcryptjs';
+import { prisma } from './db';
+import { loginInputSchema } from '../schemas/user';
 
 /**
- * Wrapper for unstable_getServerSession, used in trpc createContext and the
- * restricted API route
- *
- * Don't worry too much about the "unstable", it's safe to use but the syntax
- * may change in future versions
- *
- * @see https://next-auth.js.org/configuration/nextjs
- */
+ * Module augmentation for `next-auth` types
+ * Allows us to add custom properties to the `session` object
+ * and keep type safety
+ * @see https://next-auth.js.org/getting-started/typescript#module-augmentation
+ **/
+declare module 'next-auth' {
+  interface Session {
+    user?: {
+      id: string;
+      verified: boolean;
+    } & DefaultSession['user'];
+  }
 
+  // interface User {
+  //   // ...other properties
+  //   // role: UserRole;
+  // }
+}
+
+/**
+ * Options for NextAuth.js used to configure
+ * adapters, providers, callbacks, etc.
+ * @see https://next-auth.js.org/configuration/options
+ **/
+export const authOptions: NextAuthOptions = {
+  callbacks: {
+    session({ session, token }) {
+      if (token) {
+        // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment, @typescript-eslint/no-explicit-any
+        session.user = token.user as any;
+      }
+      return session;
+    },
+    jwt({ token, user }) {
+      if (user) {
+        token.user = user;
+      }
+      return token;
+    },
+  },
+  adapter: PrismaAdapter(prisma),
+  session: {
+    strategy: 'jwt',
+  },
+  jwt: {
+    maxAge: 15 * 24 * 30 * 60, // 15 days
+  },
+  providers: [
+    CredentialsProvider({
+      credentials: {
+        email: {},
+        password: {},
+      },
+      async authorize(credentials) {
+        const { email, password } = await loginInputSchema.parseAsync(credentials);
+
+        if (email === '' || password === '') {
+          return null;
+        }
+
+        try {
+          const user = await prisma.user.findUnique({
+            where: {
+              email,
+            },
+          });
+
+          if (!user) {
+            return null;
+          }
+
+          const passwordCorrect = await compare(password, user.password);
+          if (!passwordCorrect) {
+            return null;
+          }
+
+          return {
+            id: user.id,
+            name: user.name,
+            email: user.email,
+            image: user.image,
+            verified: user.verified,
+          };
+        } catch (e) {
+          return null;
+        }
+      },
+    }),
+  ],
+  pages: {
+    signIn: '/login',
+    newUser: '/register',
+  },
+};
+
+/**
+ * Wrapper for getServerSession so that you don't need
+ * to import the authOptions in every file.
+ * @see https://next-auth.js.org/configuration/nextjs
+ **/
 export async function getServerAuthSession(ctx: {
   req: GetServerSidePropsContext['req'];
   res: GetServerSidePropsContext['res'];
 }) {
-  return await unstable_getServerSession(ctx.req, ctx.res, authOptions);
+  return await getServerSession(ctx.req, ctx.res, authOptions);
 }
