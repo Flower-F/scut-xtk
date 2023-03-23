9d4f1ceced21ae87b4ab760aae9f80019389336d
FlowerF <1647231392@qq.com>
2023-03-23 16:12:08 +0800

    chore: project init

---
+++ prettier.config.cjs
@@ -0,0 +1,9 @@
+/** @type {import("prettier").Config} */
+const config = {
+  singleQuote: true,
+  jsxSingleQuote: true,
+  printWidth: 120,
+  plugins: [require.resolve('prettier-plugin-tailwindcss')],
+};
+
+module.exports = config;
